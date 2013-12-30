cf983ea45f553653851b8f909c7377f943defc9e
Joe <jozefnagyjr@gmail.com>
2013-12-29 22:02:44 -0500

    updated comments

--- sync.py
+++ sync.py
@@ -24,6 +24,9 @@ while i <> 0:
 	#get oldest record
 	jdata = coll.find().sort({"_id":1})
 	#How to get the _ID value of that record? Need it to delete the record after uploading it to API
+	#The _ID value is the first item in the array. Pull it from there to get it. 
+	#Also, need to remove the _ID item from the array as that's not needed when sending data to the API. Or can
+	#I just leave it and allow the API to ignore it? 
 	urllib2.urlopen(WSURL+"save.php", jdata)
 	#confirm the record was received by checking the API's return code. If so, delete the record from Mongo
 	coll.remove({"_id":"INSERT THE ID HERE"})
