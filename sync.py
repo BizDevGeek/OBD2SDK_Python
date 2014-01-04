b06cccf50934f6a4ae0195912a8f35bb6132b3e7
Joe <jozefnagyjr@gmail.com>
2014-01-04 10:42:50 -0500

    sync runs in an infinite loop

--- sync.py
+++ sync.py
@@ -26,30 +26,32 @@ def IsConnected(URL):
 	return True
 
 
-while IsConnected(WSURLConnectTest):
+while True:
 
-	client = MongoClient()
-	db = client[mongodb]
-	coll = db[mongocollection]
+	while IsConnected(WSURLConnectTest):
 
-	i = coll.count()
+		client = MongoClient()
+		db = client[mongodb]
+		coll = db[mongocollection]
+
+		i = coll.count()
 	
 	
-	while i <> 0:
+		while i <> 0:
 
-		if IsConnected(WSURLConnectTest):
-			#get oldest record
-			#data = coll.find().sort({_id:1})
-			data = coll.find_one()
-			id = data['_id']
-	#Convert from dict data type retunred by coll.find() into a JSON list data type 
-	#Also, remove the _ID item from the array as that's not needed when sending data to the API. 
-			jarray = {"APIKey":data['APIKey'], "PID":data['PID'], "PIDValue":data['PIDValue'], "EventDate":data['EventDate']}
-			jdata = json.dumps(jarray)
-			urllib2.urlopen(WSURL+"save.php", jdata)
-	#confirm the record was received by checking the API's return code. If so, delete the record from Mongo
-			coll.remove({"_id":id})
-			i = coll.count()
+			if IsConnected(WSURLConnectTest):
+				#get oldest record
+				#data = coll.find().sort({_id:1})
+				data = coll.find_one()
+				id = data['_id']
+				#Convert from dict data type retunred by coll.find() into a JSON list data type 
+				#Also, remove the _ID item from the array as that's not needed when sending data to the API. 
+				jarray = {"APIKey":data['APIKey'], "PID":data['PID'], "PIDValue":data['PIDValue'], "EventDate":data['EventDate']}
+				jdata = json.dumps(jarray)
+				urllib2.urlopen(WSURL+"save.php", jdata)
+				#confirm the record was received by checking the API's return code. If so, delete the record from Mongo
+				coll.remove({"_id":id})
+				i = coll.count()
 
 	#jdata = {"APIKey":APIKey, "PID":PID, "PIDValue":PIDValue, "EventDate":"2014-01-01 12:00:00"}
 	#client = MongoClient()
