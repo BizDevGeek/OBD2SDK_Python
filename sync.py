ac795076578d88e35cb1bf11e3d184da6c795809
Joe <jozefnagyjr@gmail.com>
2014-01-04 09:34:58 -0500

    added a check that connection exists to server, haven't tested it yet

--- sync.py
+++ sync.py
@@ -12,27 +12,30 @@ WSURL = "http://23.239.10.88/obdapi/"
 mongodb = "obd"
 mongocollection = "pids"
 
-client = MongoClient()
-db = client[mongodb]
-coll = db[mongocollection]
+while Connected(WSURL):
 
-i = coll.count()
+	client = MongoClient()
+	db = client[mongodb]
+	coll = db[mongocollection]
 
+	i = coll.count()
+	
+	
+	while i <> 0:
 
-while i <> 0:
-
-	#get oldest record
-	#data = coll.find().sort({_id:1})
-	data = coll.find_one()
-	id = data['_id']
+		if Connected(WSURL):
+			#get oldest record
+			#data = coll.find().sort({_id:1})
+			data = coll.find_one()
+			id = data['_id']
 	#Convert from dict data type retunred by coll.find() into a JSON list data type 
 	#Also, remove the _ID item from the array as that's not needed when sending data to the API. 
-	jarray = {"APIKey":data['APIKey'], "PID":data['PID'], "PIDValue":data['PIDValue'], "EventDate":data['EventDate']}
-	jdata = json.dumps(jarray)
-	urllib2.urlopen(WSURL+"save.php", jdata)
+			jarray = {"APIKey":data['APIKey'], "PID":data['PID'], "PIDValue":data['PIDValue'], "EventDate":data['EventDate']}
+			jdata = json.dumps(jarray)
+			urllib2.urlopen(WSURL+"save.php", jdata)
 	#confirm the record was received by checking the API's return code. If so, delete the record from Mongo
-	coll.remove({"_id":id})
-	i = coll.count()
+			coll.remove({"_id":id})
+			i = coll.count()
 
 	#jdata = {"APIKey":APIKey, "PID":PID, "PIDValue":PIDValue, "EventDate":"2014-01-01 12:00:00"}
 	#client = MongoClient()
@@ -42,3 +45,13 @@ while i <> 0:
 
 
 
+def Connected(URL):
+	try:
+		urllib2.urlopen(URL)
+		return true
+	except urllib2.HTTPError, e:
+    		#print e.code
+		return false
+	except urllib2.URLError, e:
+		#print e.args
+		return false
