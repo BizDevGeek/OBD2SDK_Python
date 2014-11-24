3448908da436d08d060a579c8cf54354b6ff6f9a
Jozef Nagy <jozefnagyjr@gmail.com>
2014-11-24 16:37:10 -0500

    added apache + PHP

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ echo
 
 cd /home/pi
 sudo apt-get install -y htop bwm-ng gpsd gpsd-clients python-gps sqlite3 python-serial speedometer
+sudo apt-get install -y apache2 php5 libapache2-mod-php5 php5-sqlite
 sudo gpsctl -f -n /dev/ttyUSB0
 sudo rm -rf /home/pi/OBD2SDK_Python
 git clone https://github.com/BizDevGeek/OBD2SDK_Python.git
